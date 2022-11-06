649c5daebff69a26c03dff039c54eabc34f7b7a2
= <haziqs2001@gmail.com>
2022-11-06 16:43:33 +0500

    changed Subscription model, added method body in chat

--- app_server/models/Subscription.js
+++ app_server/models/Subscription.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var Subscription = new Schema({
+var SubscriptionModel = new Schema({
     _id:{type: Number, required: true},
-    
+    p_id : {type: Number, required: true, ref:'Packages'},
+    u_id: {type: Number, required: true, ref:'User'},
+    start: {type: Date, required: true},
+    end: {type: Date, required: true},
 });
+
+module.exports = mongoose.model('Subscription',SubscriptionModel);
